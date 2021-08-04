ccd82877ccb8393143028ce71429d72c43efb6d6
Wang Long <17671087149@163.com>
2021-08-04 18:33:13 +0800

    完成登录逻辑

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 from socket import *
-from deal_sigin_up import *
+
 import pymysql
-import threading
+import gevent
+
+from deal_sigin_up import *
+from deal_login import *
+from deal_chat_send import *
+from deal_chat_receive import *
+
+# 在线的客户端
+online_clients = list()
 
 
-def deal_data(client, database, data):
-    if data == SIGN_UP_SIGNAL:
-        # 此时表面是注册请求
-        # 交给注册函数解决
-        deal_sign_up(client, database)
-    else:
-        pass
+def deal_data(client, database):
+    global online_clients
+    while True:
+        data = None
+        try:
+            data_b = client.recv(1024)
+            data = data_b.decode('utf-8')
+        except Exception as e:
+            pass
+        # 检查数据
+        if not data:
+            client.close()
+            if online_clients.__contains__(client):
+                online_clients.remove(client)
+            return
+
+        header_and_body = data.split(REQUEST_SEPARATOR)
+
+        header = header_and_body[0]
+        body = None
+        if len(header_and_body) > 1:
+            body = header_and_body[1]
+
+        if header == REQUEST_HEADER_SIGN_UP:
+            # 此时是注册请求
+            deal_sign_up(client, database, body)
+        elif header == REQUEST_HEADER_LOGIN:
+            # 此时是登录请求
+            deal_login(client, database, body)
+        elif header == REQUEST_HEADER_CHAT_CLIENT_LOGIN:
+            # 此时是聊天客户端登录请求
+            # 添加该客户端
+            online_clients.append(client)
+            print("有用户接入")
+        elif header == REQUEST_HEADER_CHAT_SEND:
+            # 此时是发送信息请求
+            deal_chat_send(client, database, body, online_clients)
+        elif header == REQUEST_HEADER_CHAT_RECEIVE:
+            # 此时是接收请求
+            # deal_chat_receive(client, database, body)
+            client.send(b"RECEIVE")
+        else:
+            pass
 
 
 def start_server():
@@ -24,8 +68,17 @@ def start_server():
 
     while True:
         client, addr = server.accept()
-        deal_thread = threading.Thread(target=deal_data, args=(client, database, client.recv(1024).decode('utf-8')))
-        deal_thread.start()
+        # try:
+        #     deal_thread = threading.Thread(target=deal_data, args=(client, database))
+        #     deal_thread.start()
+        # except Exception as e:
+        #     print(e)
+
+        try:
+            g1 = gevent.spawn(deal_data, client, database)
+            g1.join()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
